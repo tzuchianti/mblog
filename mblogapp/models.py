1ab07aa99680dd74143e01fc88cec610bb0b2d5b
tzuchianti <tzuchianti@gmail.com>
2021-04-23 09:11:45 +0800

    book test

--- mblogapp/models.py
+++ mblogapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.utils import timezone
-#now= timezone.now()
 
+
+#####################book################################
+from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
@@ -13,4 +14,25 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
+
+####################Django 3.2 doc###############################
+import datetime
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
--- mblogapp/views.py
+++ mblogapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
 # Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    context = {
+        'posts':posts,
+        'now':now,
+    }
+    return render(request, 'homepage.html', context=context)
+    
